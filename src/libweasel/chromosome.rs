f12143acf128ba7287feafba01b6e35af7b983b3
Antonio-M. Corbi Bellot <antonio.corbi@gmail.com>
2025-11-19 19:41:50 +0100

    Implemented chromosome.

--- src/libweasel/chromosome.rs
+++ src/libweasel/chromosome.rs
@@ -15,6 +15,7 @@
 
 use crate::libweasel::gene::Gene;
 use signals2::*;
+use std::ops::Index;
 
 type GeneList = Vec<Box<Gene>>;
 
@@ -41,16 +42,27 @@ impl Chromosome {
             ncopies,
             gene_list: vec![],
         };
-        c.create_genes();
+        c.create_random_genes();
 
         c
+    }
+
+    pub fn ncopies(self) -> u32 {
+        self.ncopies
     }
 
     pub fn target(&self) -> String {
         self.target_string.clone()
     }
 
-    fn create_genes(&mut self) {
+    fn create_genes_from_target(&mut self) {
+        self.free_gene_list();
+        for c in self.target_string.chars() {
+            self.gene_list.push(Box::new(Gene::new(c)));
+        }
+    }
+
+    fn create_random_genes(&mut self) {
         self.free_gene_list();
         for _ in 0..self.target_string.len() {
             self.gene_list.push(Box::new(Gene::new_from_random()));
@@ -80,8 +92,80 @@ impl Chromosome {
     }
 }
 
+impl Index<usize> for Chromosome {
+    type Output = Box<Gene>;
+
+    fn index(&self, idx: usize) -> &Self::Output {
+        &self.gene_list[idx]
+    }
+}
+
 impl Drop for Chromosome {
     fn drop(&mut self) {
         self.free_gene_list();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_index1() {
+        let mut c = Chromosome::new("hola".into(), 4);
+        c.create_genes_from_target();
+        let gc0 = <&Gene as Into<char>>::into(&*c[0]);
+        let gc1: char = (&*c[1]).into();
+
+        assert_eq!(gc0, 'h');
+        assert_eq!(gc1, 'o');
+    }
+
+    #[test]
+    fn test_fitness1() {
+        let c = Chromosome::new("hola".into(), 4);
+        let g1 = Box::new(Gene::new('h'));
+        let g2 = Box::new(Gene::new('a'));
+        let g3 = Box::new(Gene::new('l'));
+        let g4 = Box::new(Gene::new('a'));
+        let mut v = vec![];
+        v.push(g1);
+        v.push(g2);
+        v.push(g3);
+        v.push(g4);
+
+        assert_eq!(c.fitness(&v), 1);
+    }
+
+    #[test]
+    fn test_fitness2() {
+        let c = Chromosome::new("hola".into(), 4);
+        let g1 = Box::new(Gene::new('h'));
+        let g2 = Box::new(Gene::new('a'));
+        let g3 = Box::new(Gene::new('l'));
+        let g4 = Box::new(Gene::new('o'));
+        let mut v = vec![];
+        v.push(g1);
+        v.push(g2);
+        v.push(g3);
+        v.push(g4);
+
+        assert_eq!(c.fitness(&v), 2);
+    }
+
+    #[test]
+    fn test_fitness3() {
+        let c = Chromosome::new("hola".into(), 4);
+        let g1 = Box::new(Gene::new('h'));
+        let g2 = Box::new(Gene::new('o'));
+        let g3 = Box::new(Gene::new('l'));
+        let g4 = Box::new(Gene::new('a'));
+        let mut v = vec![];
+        v.push(g1);
+        v.push(g2);
+        v.push(g3);
+        v.push(g4);
+
+        assert_eq!(c.fitness(&v), 0);
     }
 }
